8ecf71e5f1a85738167ae7f99b60e6c5507237b1
Siavash Kaseb <siavashkaseb07@gmail.com>
2023-02-24 13:44:49 +0330

    SectionPart bug fixed

--- Reading/edit/scripts/SectionPart.js
+++ Reading/edit/scripts/SectionPart.js
@@ -1,27 +1,13 @@
-const sec = document.getElementById("sectionMainDiv");
-const textBox = document.getElementById("titleText");
-const btnNav = document.getElementById("navBtn");
-const title = document.getElementById("titleTop");
-let paraText = document.getElementById("paraText")
+let text = document.getElementById("mainText");
 let dark = false;
 
 const darkMode = () => {
     dark = !dark;
     if (dark) {
-        alert("true")
         document.body.style.backgroundColor = "black";
-        document.body.style.color = "white"
-        sec.style.backgroundColor = "black";
-        sec.style.color = "white";
-        paraText.style.color = "white"
-        textBox.style.color = "white !important";
-        title.style.color = "black";
+        text.style.color = "white";
     } else {
-        alert("false")
         document.body.style.backgroundColor = "white";
-        sec.style.backgroundColor = "white";
-        sec.style.color = "black";
-        textBox.style.color = "black";
-        title.style.color = "white";
+        text.style.color = "black";
     }
 };
